8cee1458cb416b2c8b77c07d18d574e24be5b64c
mhelmy12 <mhelmy@std.mans.edu.eg>
2026-09-03 20:10:37 +0300

    feat: Add Kafka and Debezium containers with configurations

--- AppHost/Program.cs
+++ AppHost/Program.cs
@@ -10,6 +10,7 @@ var sqlserver = builder.AddSqlServer("sqlserver", sqlPassword)
     .WithExternalHttpEndpoints()
     .WithEnvironment("MSSQL_SA_PASSWORD", sqlPassword)
     .WithEnvironment("ACCEPT_EULA", "Y")
+    .WithEnvironment("MSSQL_AGENT_ENABLED", "True")
     .WithLifetime(ContainerLifetime.Persistent);
 
 
@@ -33,7 +34,6 @@ var keycloak = builder.AddKeycloak("keycloak", 8082)
     .WithRealmImport("./Realms")
     .WithArgs("--features=docker,admin-fine-grained-authz,token-exchange,quick-theme")
     .WithLifetime(ContainerLifetime.Persistent)
-
     .WithUrls(context =>
         {
             foreach (var u in context.Urls)
@@ -50,6 +50,92 @@ var keycloak = builder.AddKeycloak("keycloak", 8082)
                 }
             );
         });
+
+keycloak.WaitFor(sqlserver);
+
+
+var kafka = builder.AddKafka("kafka")
+    .WithImage("confluentinc/cp-kafka", "latest")
+    .WithEnvironment("KAFKA_NODE_ID", "1")
+    .WithEnvironment("KAFKA_PROCESS_ROLES", "broker,controller")
+    .WithEnvironment("KAFKA_CONTROLLER_QUORUM_VOTERS", "1@kafka:9093")
+    .WithEnvironment("KAFKA_LISTENERS", "INTERNAL://0.0.0.0:29092,EXTERNAL://0.0.0.0:9092,CONTROLLER://0.0.0.0:9093")
+    .WithEnvironment("KAFKA_ADVERTISED_LISTENERS", "INTERNAL://kafka:29092,EXTERNAL://localhost:9092")
+    .WithEnvironment("KAFKA_LISTENER_SECURITY_PROTOCOL_MAP", "INTERNAL:PLAINTEXT,EXTERNAL:PLAINTEXT,CONTROLLER:PLAINTEXT")
+    .WithEnvironment("KAFKA_INTER_BROKER_LISTENER_NAME", "INTERNAL")
+    .WithEnvironment("KAFKA_CONTROLLER_LISTENER_NAMES", "CONTROLLER")
+    .WithEnvironment("KAFKA_OFFSETS_TOPIC_REPLICATION_FACTOR", "1")
+    .WithEnvironment("KAFKA_TRANSACTION_STATE_LOG_REPLICATION_FACTOR", "1")
+    .WithEnvironment("KAFKA_TRANSACTION_STATE_LOG_MIN_ISR", "1")
+    .WithEnvironment("CLUSTER_ID", "MkU3OEVBNTcwNTJENDM2Qk")
+    .WithVolume("kafka_data", "/var/lib/kafka/data");
+
+
+
+
+var kafkaUi = builder.AddContainer("kafka-ui", "provectuslabs/kafka-ui", "latest")
+.WithHttpEndpoint(port: 8080, targetPort: 8080, name: "http", isProxied: false)
+
+    .WithEnvironment("KAFKA_CLUSTERS_0_NAME", "local")
+    .WithEnvironment("KAFKA_CLUSTERS_0_BOOTSTRAPSERVERS", "kafka:29092")
+    .WithEnvironment("KAFKA_CLUSTERS_0_SCHEMAREGISTRY", "http://schema-registry:8081")
+    .WithReference(kafka)
+      .WithUrls(context =>
+        {
+            foreach (var u in context.Urls)
+            {
+                u.DisplayLocation = UrlDisplayLocation.DetailsOnly;
+            }
+
+            context.Urls.Add(
+                new ResourceUrlAnnotation()
+                {
+                    Url = "/",
+                    DisplayText = "Kafka UI Dashboard",
+                    Endpoint = context.GetEndpoint("http"),
+                }
+            );
+        });
+
+
+
+var debezium = builder.AddContainer("debezium", "quay.io/debezium/connect", "latest")
+    .WithEnvironment("BOOTSTRAP_SERVERS", "kafka:29092")
+    .WithEnvironment("GROUP_ID", "banking-connect")
+    .WithEnvironment("CONFIG_STORAGE_TOPIC", "connect_configs")
+    .WithEnvironment("OFFSET_STORAGE_TOPIC", "connect_offsets")
+    .WithEnvironment("STATUS_STORAGE_TOPIC", "connect_status")
+    .WithEnvironment("CONFIG_STORAGE_REPLICATION_FACTOR", "1")
+    .WithEnvironment("OFFSET_STORAGE_REPLICATION_FACTOR", "1")
+    .WithEnvironment("STATUS_STORAGE_REPLICATION_FACTOR", "1")
+    .WithReference(kafka)
+    .WithReference(sqlserver)
+    .WithHttpEndpoint(port: 8083, targetPort: 8083, name: "http", isProxied: false)
+    .WithExternalHttpEndpoints();
+
+var debeziumUi = builder.AddContainer("debezium-ui", "debezium/debezium-ui", "latest")
+    .WithEnvironment("KAFKA_CONNECT_URIS", debezium.GetEndpoint("http"))
+    .WithHttpEndpoint(port: 8085, targetPort: 8080, name: "http", isProxied: false)
+
+    .WithExternalHttpEndpoints()
+      .WithUrls(context =>
+        {
+            foreach (var u in context.Urls)
+            {
+                u.DisplayLocation = UrlDisplayLocation.DetailsOnly;
+            }
+
+            context.Urls.Add(
+                new ResourceUrlAnnotation()
+                {
+                    Url = "/",
+                    DisplayText = "Debezium Dashboard",
+                    Endpoint = context.GetEndpoint("http"),
+                }
+            );
+        });
+debezium.WaitFor(kafka).WaitFor(sqlserver);
+
 
 var userService = builder.AddProject<UserService>("user-service");
 
